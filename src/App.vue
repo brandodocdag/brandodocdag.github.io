b5c88fea28869627de2ec339a43ae0f4758a8713
brandodocdag <brando.docdag@gmail.com>
2019-07-07 08:08:51 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,232 @@
+<template>
+  <div id="app">
+    <StyleEditor ref="styleEditor" :code="currentStyle"></StyleEditor>
+    <ResumeEditor ref="resumeEditor" :markdown="currentMarkdown" :enableHtml="enableHtml"></ResumeEditor>
+  </div>
+</template>
+
+<script>
+  import StyleEditor from './components/StyleEditor'
+  import ResumeEditor from './components/ResumeEditor'
+  import './assets/reset.css'
+
+  export default {
+    name: 'app',
+    components: {
+      StyleEditor,
+      ResumeEditor
+    },
+    data() {
+      return {
+        interval: 40,
+        currentStyle: '',
+        enableHtml: false,
+        fullStyle: [
+          `/*
+* Inspired by http://strml.net/
+* Halo semuanya, saya Brando Sijabat
+* Pada bulan Februari, banyak perusahaan yang mempekerjakan, apakah Anda mempersiapkan resume Anda?
+* Lakukan saja, saya juga akan menulis resume!
+*/
+
+/* Pertama, tambahkan efek transisi ke semua elemen*/
+* {
+  transition: all .3s;
+}
+/* Latar belakang putih terlalu monoton, mari klik pada latar belakang */
+html {
+  color: rgb(222,222,222); background: rgb(0,43,54);
+}
+/* Teks terlalu dekat dengan perbatasan */
+.styleEditor {
+  padding: .5em;
+  border: 1px solid;
+  margin: .5em;
+  overflow: auto;
+  width: 45vw; height: 90vh;
+}
+/* Menyoroti kode */
+.token.selector{ color: rgb(133,153,0); }
+.token.property{ color: rgb(187,137,0); }
+.token.punctuation{ color: yellow; }
+.token.function{ color: rgb(42,161,152); }
+
+/* Tambahkan efek 3D */
+html{
+  perspective: 1000px;
+}
+.styleEditor {
+  position: fixed; left: 0; top: 0;
+  -webkit-transition: none;
+  transition: none;
+  -webkit-transform: rotateY(10deg) translateZ(-100px) ;
+          transform: rotateY(10deg) translateZ(-100px) ;
+}
+
+/* Selanjutnya saya menyiapkan editor untuk diri saya sendiri */
+.resumeEditor{
+  position: fixed; right: 0; top: 0;
+  padding: .5em;  margin: .5em;
+  width: 48vw; height: 90vh;
+  border: 1px solid;
+  background: white; color: #222;
+  overflow: auto;
+}
+/*Ok, saya mulai menulis resume saya. */
+
+
+`,
+          `
+/*Resume ini tampaknya hampir seperti apa
+ * Ya, ini dalam format penurunan harga, saya harus menjadi format yang lebih ramah untuk SDM
+ * Sederhana, gunakan alat open source untuk menerjemahkan ke dalam HTML.
+ */
+`
+          ,
+          `
+/*Tambahkan gaya ke HTML */
+.resumeEditor{
+  padding: 2em;
+}
+.resumeEditor h2{
+  display: inline-block;
+  border-bottom: 1px solid;
+  margin: 1em 0 .5em;
+}
+.resumeEditor ul,.resumeEditor ol{
+  list-style: none;
+}
+.resumeEditor ul> li::before{
+  content: '•';
+  margin-right: .5em;
+}
+.resumeEditor ol {
+  counter-reset: section;
+}
+.resumeEditor ol li::before {
+  counter-increment: section;
+  content: counters(section, ".") " ";
+  margin-right: .5em;
+}
+.resumeEditor blockquote {
+  margin: 1em;
+  padding: .5em;
+  background: #ddd;
+}
+`],
+        currentMarkdown: '',
+        fullMarkdown: `方应杭
+----
+
+Insinyur senior front-end, senior front-end dosen, sekarang mengajar kursus front-end di [Hungry Valley] (http://jirengu.com)。
+
+Ketrampilan
+----
+
+* Pengembangan front-end
+* Pengembangan rel
+* Node.js
+* Kuliah front-end
+
+Pengalaman kerja
+----
+
+1. [Hungry Valley] (http://jirengu.com)
+2. Platform Pesan Instan Tencent
+3. Departemen Alibaba B2B
+4. Tim Proyek Warna Cheng Zhiren
+
+Tautan
+----
+
+* [GitHub](https://github.com/frankfang)
+* [Artiketl Saya](https://www.zhihu.com/people/zhihusucks/pins/posts)
+
+> Jika Anda menyukai efek ini, Fork [Proyek Saya] (https://github.com/jirengu-inc/animating-resume), buat resume Anda sendiri!
+
+`
+      }
+    },
+    created() {
+      this.makeResume()
+    },
+
+    methods: {
+      makeResume: async function () {
+        await this.progressivelyShowStyle(0)
+        await this.progressivelyShowResume()
+        await this.progressivelyShowStyle(1)
+        await this.showHtml()
+        await this.progressivelyShowStyle(2)
+      },
+      showHtml: function () {
+        return new Promise((resolve, reject) => {
+          this.enableHtml = true
+          resolve()
+        })
+      },
+      progressivelyShowStyle(n) {
+        return new Promise((resolve, reject) => {
+          let interval = this.interval
+          let showStyle = (async function () {
+            let style = this.fullStyle[n]
+            if (!style) { return }
+            // Sebelum perhitungan n 个 style Total jumlah karakter
+            let length = this.fullStyle.filter((_, index) => index <= n).map((item) => item.length).reduce((p, c) => p + c, 0)
+            let prefixLength = length - style.length
+            if (this.currentStyle.length < length) {
+              let l = this.currentStyle.length - prefixLength
+              let char = style.substring(l, l + 1) || ' '
+              this.currentStyle += char
+              if (style.substring(l - 1, l) === '\n' && this.$refs.styleEditor) {
+                this.$nextTick(() => {
+                  this.$refs.styleEditor.goBottom()
+                })
+              }
+              setTimeout(showStyle, interval)
+            } else {
+              resolve()
+            }
+          }).bind(this)
+          showStyle()
+        })
+      },
+      progressivelyShowResume() {
+        return new Promise((resolve, reject) => {
+          let length = this.fullMarkdown.length
+          let interval = this.interval
+          let showResume = () => {
+            if (this.currentMarkdown.length < length) {
+              this.currentMarkdown = this.fullMarkdown.substring(0, this.currentMarkdown.length + 1)
+              let lastChar = this.currentMarkdown[this.currentMarkdown.length - 1]
+              let prevChar = this.currentMarkdown[this.currentMarkdown.length - 2]
+              if (prevChar === '\n' && this.$refs.resumeEditor) {
+                this.$nextTick(() => this.$refs.resumeEditor.goBottom())
+              }
+              setTimeout(showResume, interval)
+            } else {
+              resolve()
+            }
+          }
+          showResume()
+        })
+      }
+    }
+  }
+
+</script>
+
+<style scoped>
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+
+  html {
+    min-height: 100vh;
+  }
+  *{
+    box-sizing: border-box;
+  }
+</style>
